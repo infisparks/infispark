ad72c985d4814b5acd8156d2034c25ef4fc19f7e
MK MUZAKKIR <muzakkir7977@gmail.com>
2025-01-25 20:44:48 -0500

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Contact from './pages/Contact.jsx';
